b8ece15a2783037bd15853c9fc7074ff262f7a0d
Paul Potsides <strongpauly@hotmail.com>
2019-06-09 09:55:20 +0100

    npm audit

--- src/Work.js
+++ src/Work.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
-import moment from 'moment';
+import React, { Component } from "react";
+import propTypes from "prop-types";
+import moment from "moment";
 
 class Work extends Component {
-
   static propTypes = {
     work: propTypes.object
-  }
+  };
 
-  formatDate ( entry ) {
-    return moment(entry).format('MMM YY');
+  formatDate(entry) {
+    return moment(entry).format("MMM YY");
   }
-
 
   render() {
     const work = this.props.work;
     let company = work.company;
-    if(work.website !== undefined) {
-      company = <a href={work.website} target="_blank">{company}</a>;
+    if (work.website !== undefined) {
+      company = (
+        <a href={work.website} target="_blank" rel="noopener noreferrer">
+          {company}
+        </a>
+      );
     }
     let highlights;
-    if(work.highlights.length > 0) {
-      highlights=<ul>{
-        work.highlights.map((highlight, index) => <li key={index}>{highlight}</li>)
-      }
-      </ul>;
+    if (work.highlights.length > 0) {
+      highlights = (
+        <ul>
+          {work.highlights.map((highlight, index) => (
+            <li key={index}>{highlight}</li>
+          ))}
+        </ul>
+      );
     }
     return (
       <div>
-        <h6>{company} <small>{this.formatDate(work.startDate)} to {this.formatDate(work.endDate)} <i>{work.position}</i></small></h6>
+        <h6>
+          {company}{" "}
+          <small>
+            {this.formatDate(work.startDate)} to {this.formatDate(work.endDate)}{" "}
+            <i>{work.position}</i>
+          </small>
+        </h6>
         <div>{work.summary}</div>
         {highlights}
       </div>
